fc6433e5e1a0fbd7defba45c116e2ebc09228aef
alexandrabotvinova1406-del <alexandra.botvinova.1406@gmail.com>
2026-06-06 02:32:37 +0300

    Keysolo_task_test

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,18 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keyup', (event) => {
+    const inputChar = event.key;
+    
+    if (inputChar.length !== 1) return;
+    const currentChar = this.currentSymbol.textContent;
+    
+    if (inputChar.toLowerCase() === currentChar.toLowerCase()) {
+      this.success();
+    } else {
+      this.fail();
+    }
+  });
   }
 
   success() {
